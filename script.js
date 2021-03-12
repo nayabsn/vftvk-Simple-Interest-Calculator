7e944e31512326f965c4330a7d7d3dbe303a1036
nayabsn <60349507+nayabsn@users.noreply.github.com>
2021-03-12 12:40:56 +0400

    Update script.js
    Update 2

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    document.getElementById("interest").innerHTML = principal * years * rate / 100;
+    document.getElementById("futureYear").innerHTML = 2021 + years;
 }
-        
+
+function slider ()
+{
+    var rangeslider = document.getElementById("sliderRange");
+    var output = document.getElementById("out");
+    output.innerHTML = rangeslider.value;
+
+    rangeslider.oninput = function(){
+        output.innerHTML = this.value;
+    }
+}
