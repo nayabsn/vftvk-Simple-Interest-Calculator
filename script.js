3756040d40d6eafd371b295871b0fa45fe21f08c
nayabsn <60349507+nayabsn@users.noreply.github.com>
2021-03-13 16:05:14 +0400

    final script.js
    Final version

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-function compute()
+// This function will display the range value for the "Interest Rate" selected by the user
+function findRange()
 {
-    principal = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    years = document.getElementById("years").value;
-    document.getElementById("interest").innerHTML = principal * years * rate / 100;
-    document.getElementById("futureYear").innerHTML = 2021 + years;
+    var slider1 = document.getElementById("myRange");
+    var demo = document.getElementById("demo");
+    demo.innerHTML = slider1.value;
+
+    slider1.oninput = function() {
+    demo.innerHTML = this.value;
+    }
 }
 
-function slider ()
+// This function will read and return the values entered by the user and calculate the year and interest
+function compute()
 {
-    var rangeslider = document.getElementById("sliderRange");
-    var output = document.getElementById("out");
-    output.innerHTML = rangeslider.value;
+    var p = Number(document.getElementById("a").value);
+    var n = Number(document.getElementById("b").value);
+    var r = Number(document.getElementById("myRange").value);
 
-    rangeslider.oninput = function(){
-        output.innerHTML = this.value;
+    // Validation check for the value entered for amount is >0
+    if (p <= 0)
+    {
+        alert("Enter a positive number");
     }
+
+    else {
+        var result = 0;
+        var futureYear = 0;
+        var amount = p;
+        var rate = r;
+    
+        result=(p*r*n/100);
+        futureYear = 2021 + n;
+        
+        document.getElementById("result").innerHTML = result;
+        document.getElementById("futureYear").innerHTML = futureYear;
+        document.getElementById("amount").innerHTML = amount;
+        document.getElementById("rate").innerHTML = rate;
+    }
+   
 }
